aacdee0bfa4d7b4e168c81970d9ae8c34d2008d1
Banee Ishaque K <baneeishaque@gmail.com>
2025-11-01 21:03:42 +0530

    Improves type safety for activity parameters
    Constrains the `nextActivity` parameter in `CheckAndUpdateTaskWrapper` methods to `Class`.
    This ensures only compatible activity types are passed, preventing potential runtime errors and enhancing code robustness.

--- common19/src/main/java/ndk/utils_android19/network_task/update/CheckAndUpdateTaskWrapper.java
+++ common19/src/main/java/ndk/utils_android19/network_task/update/CheckAndUpdateTaskWrapper.java
@@ -9,22 +9,22 @@ public class CheckAndUpdateTaskWrapper {
 
     //TODO : Get Task Objects
 
-    public static void executeCheckAndUpdateWithTabIndexTask(String applicationName, AppCompatActivity currentActivity, String url, String updateUrl, Class nextActivity, boolean securityFlag, int tabIndex, PairOfStringsModel[] nextClassExtras) {
+    public static void executeCheckAndUpdateWithTabIndexTask(String applicationName, AppCompatActivity currentActivity, String url, String updateUrl, Class<? extends AppCompatActivity> nextActivity, boolean securityFlag, int tabIndex, PairOfStringsModel[] nextClassExtras) {
         CheckAndUpdateTask checkAndUpdateTask = new CheckAndUpdateTask(applicationName, currentActivity, url, updateUrl, nextActivity, securityFlag, true, tabIndex, nextClassExtras);
 
         checkAndUpdateTask.execute();
     }
 
-    public static void executeCheckAndUpdateWithZeroTabIndexTask(String applicationName, AppCompatActivity currentActivity, String url, String updateUrl, Class nextActivity, boolean securityFlag, PairOfStringsModel[] nextClassExtras) {
+    public static void executeCheckAndUpdateWithZeroTabIndexTask(String applicationName, AppCompatActivity currentActivity, String url, String updateUrl, Class<? extends AppCompatActivity> nextActivity, boolean securityFlag, PairOfStringsModel[] nextClassExtras) {
         executeCheckAndUpdateWithTabIndexTask(applicationName, currentActivity, url, updateUrl, nextActivity, securityFlag, 0, nextClassExtras);
     }
 
-    public static void executeCheckAndUpdateWithoutTabIndexTask(String applicationName, AppCompatActivity currentActivity, String url, String updateUrl, Class nextActivity, boolean securityFlag, PairOfStringsModel[] nextClassExtras) {
+    public static void executeCheckAndUpdateWithoutTabIndexTask(String applicationName, AppCompatActivity currentActivity, String url, String updateUrl, Class<? extends AppCompatActivity> nextActivity, boolean securityFlag, PairOfStringsModel[] nextClassExtras) {
 
         getCheckAndUpdateWithoutTabIndexTask(applicationName, currentActivity, url, updateUrl, nextActivity, securityFlag, nextClassExtras).execute();
     }
 
-    public static CheckAndUpdateTask getCheckAndUpdateWithoutTabIndexTask(String applicationName, AppCompatActivity currentActivity, String url, String updateUrl, Class nextActivity, boolean securityFlag, PairOfStringsModel[] nextClassExtras) {
+    public static CheckAndUpdateTask getCheckAndUpdateWithoutTabIndexTask(String applicationName, AppCompatActivity currentActivity, String url, String updateUrl, Class<? extends AppCompatActivity> nextActivity, boolean securityFlag, PairOfStringsModel[] nextClassExtras) {
 
         return new CheckAndUpdateTask(applicationName, currentActivity, url, updateUrl, nextActivity, securityFlag, false, 0, nextClassExtras);
     }
